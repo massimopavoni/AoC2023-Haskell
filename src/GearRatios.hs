9ccd7abf9f65362ac17e3fcdce2b6df681036764
Massimo Pavoni <maspavoni@gmail.com>
2023-12-05 21:40:22 +0100

    GearRatios was a blast, but also an inferno

---
+++ src/GearRatios.hs
@@ -0,0 +1,124 @@
+{-# LANGUAGE TupleSections #-}
+
+module GearRatios (partNumbers, gearRatios) where
+
+import Control.Arrow ((&&&))
+import Control.Category ((>>>))
+import Control.Monad (guard)
+import Data.Functor ((<&>))
+import Data.Map.Strict (elems, fromListWith)
+import qualified Data.Map.Strict as Map (filter)
+import Data.Matrix (Matrix, fromLists, safeGet)
+import Data.Maybe (catMaybes, mapMaybe)
+import Data.Set (fromList, toList)
+import Data.Tuple.Extra (both)
+import Data.Void (Void)
+import Text.Megaparsec (Parsec, between, eof, errorBundlePretty, getInput, getSourcePos, many, noneOf, optional, parse, some, someTill, sourceColumn, sourceLine, unPos)
+import Text.Megaparsec.Char (digitChar, newline)
+
+-- This problem was...oh so much more challenging.
+-- Took me three days, with all the time I could spare, to get it right.
+-- It might not be the most elegant solution, but it's mine, I like it,
+-- it made me learn many more things, and it doesn't look too bad.
+
+-- The first part is the most important, I had to find a way of dealing with the grid-like input.
+partNumbers :: String -> [Integer]
+partNumbers = either (error . errorBundlePretty) id . parse enginePartsParser ""
+
+type Parser = Parsec Void String
+
+-- I used megaparsec again, just because in my mind there was no way
+-- I was gonna be able to correcly parse the parts numbers and
+-- the near symbols positions without graually working through the matrix.
+enginePartsParser :: Parser [Integer]
+enginePartsParser = do
+  m <- fromLists . lines <$> getInput
+  catMaybes <$> someTill (maybePart m) eof
+
+-- The maybePart parser searches for a sequence of digits between dots and other symbols,
+-- then uses the provided function to decide if the number is a part number.
+-- The one thing that allows this is checking every neighbor of every digit,
+-- and that's only possible thanks to the position function.
+maybePart :: Matrix Char -> Parser (Maybe Integer)
+maybePart m = betweenSymbols nearSymbol
+  where
+    nearSymbol :: Maybe [((Int, Int), Char)] -> Maybe Integer
+    nearSymbol = maybe Nothing $ \z -> do
+      guard $ any (any symbolNeighbor . neighbors m . fst) z
+      Just . read $ snd <$> z
+
+    symbolNeighbor :: ((Int, Int), Char) -> Bool
+    symbolNeighbor = (`notElem` '\n' : '.' : ['0' .. '9']) . snd
+
+betweenSymbols :: (Maybe [((Int, Int), Char)] -> Maybe b) -> Parser (Maybe b)
+betweenSymbols f =
+  between (many noDigit) (many noDigit <* optional newline) $
+    f <$> optional (some $ (,) <$> position <*> digitChar)
+
+noDigit :: Parser Char
+noDigit = noneOf $ '\n' : ['0' .. '9']
+
+-- The position function allowed me to use a more interesting Arrow combinator
+-- for the first time, and I'm glad I did, 'cause it looks sick.
+-- In all seriousness, the position function could be a bit of a controversial choice,
+-- since it's supposedly "not cheap", in terms of computation time.
+-- It's true that I don't call it on every single character, and that it's necessary
+-- for way I represented the engine and solve the entire problem,
+-- but I'm sure there's a better solution.
+position :: Parser (Int, Int)
+position = both unPos . (sourceLine &&& sourceColumn) <$> getSourcePos
+
+-- Neighbors is maybe the least complex function of all of this,
+-- and it got slightly better once I decided to go back to the Matrix type,
+-- instead of using an Array, since the first one had consistent indexing bounds
+-- in relation to the way megaparsec gives you the current source position.
+neighbors :: Matrix Char -> (Int, Int) -> [((Int, Int), Char)]
+neighbors m = mapMaybe safeNeighbor . positions
+  where
+    safeNeighbor :: (Int, Int) -> Maybe ((Int, Int), Char)
+    safeNeighbor p = (p,) <$> uncurry safeGet p m
+
+    positions :: (Int, Int) -> [(Int, Int)]
+    positions (x, y) = [(l, c) | l <- [x - 1 .. x + 1], c <- [y - 1 .. y + 1], (l, c) /= (x, y)]
+
+-- The second part was quite a lot easier, considering the amount of hours I spent on the first one.
+gearRatios :: String -> [Integer]
+gearRatios =
+  either (error . errorBundlePretty) id . parse engineGearsParser ""
+    >>> map product
+
+-- The engineGearsParser does a tiny bit more than what the above enginePartsParser does.
+-- It's a crucial part of the problem, which I actually solved quite elegantly, in my opinion:
+-- the function applied to the tokens parsed by multiple maybeGears is essentially a pipeline that
+-- 1. filters out all the Nothing values (the numbers that are not parts or the parts that are not near gears)
+-- 2. creates a Map with gears positions to lists of their near parts (effectively grouping the parts to be then multiplied)
+-- 3. filters out all gears that could not result in a ratio (the ones that don't have exactly two near parts)
+-- 4. gets the values of the Map
+engineGearsParser :: Parser [[Integer]]
+engineGearsParser = do
+  m <- fromLists . lines <$> getInput
+  someTill (maybeGears m) eof
+    <&> ( catMaybes
+            >>> fromListWith (++) . map (pure <$>) . concat
+            >>> Map.filter ((== 2) . length)
+            >>> elems
+        )
+
+-- Finally, the maybeGears parser is the equivalent of the maybePart parser,
+-- just for the second part of the problem.
+-- Nothing too strange here, other than the final result not containing just one part number,
+-- but a list of gear positions (to later use for grouping) and corresponding part numbers.
+maybeGears :: Matrix Char -> Parser (Maybe [((Int, Int), Integer)])
+maybeGears m = betweenSymbols nearGear
+  where
+    nearGear :: Maybe [((Int, Int), Char)] -> Maybe [((Int, Int), Integer)]
+    nearGear = maybe Nothing $ \z -> do
+      let gears = gearNeighbors z
+      guard . not . null $ gears
+      Just $ (,read $ snd <$> z) <$> gears
+
+    gearNeighbors :: [((Int, Int), Char)] -> [(Int, Int)]
+    gearNeighbors =
+      concatMap (neighbors m . fst)
+        >>> toList . fromList
+        >>> fmap fst . filter ((== '*') . snd)
--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,9 @@
-module Main (main, trebuchetSolution, cubeConundrumSolution) where
+module Main (main, trebuchetSolution, cubeConundrumSolution, gearRatiosSolution) where
 
+import Control.Exception (assert)
 import CubeConundrum (CubeColor (..), fewestCubes, possibleGame)
 import Data.Maybe (mapMaybe)
-import GearRatios (partNumbers)
+import GearRatios (gearRatios, partNumbers)
 import Trebuchet (retrieveCalibration, retrieveCalibrationFixed)
 
 main :: IO ()
@@ -14,22 +15,41 @@ main = do
 
 trebuchetSolution :: IO ()
 trebuchetSolution = do
-  putStr "Trebuchet one -> "
-  print . sum . map retrieveCalibration . lines =<< readFile "src/resources/Trebuchet.in"
-  putStr "Trebuchet two -> "
-  print . sum . map retrieveCalibrationFixed . lines =<< readFile "src/resources/Trebuchet.in"
+  solutionPretty
+    ("Trebuchet", 1)
+    (sum . map retrieveCalibration . lines, 54388)
+    "src/resources/Trebuchet.in"
+  solutionPretty
+    ("Trebuchet", 2)
+    (sum . map retrieveCalibrationFixed . lines, 53515)
+    "src/resources/Trebuchet.in"
   putStrLn ""
 
 cubeConundrumSolution :: IO ()
 cubeConundrumSolution = do
-  putStr "CubeConundrum one -> "
-  print . sum . mapMaybe (possibleGame [(Blue, 14), (Green, 13), (Red, 12)]) . lines =<< readFile "src/resources/CubeConundrum.in"
-  putStr "CubeConundrum two -> "
-  print . sum . map (product . map snd . fewestCubes) . lines =<< readFile "src/resources/CubeConundrum.in"
+  solutionPretty
+    ("CubeConundrum", 1)
+    (sum . mapMaybe (possibleGame [(Blue, 14), (Green, 13), (Red, 12)]) . lines, 2278)
+    "src/resources/CubeConundrum.in"
+  solutionPretty
+    ("CubeConundrum", 2)
+    (sum . map (product . map snd . fewestCubes) . lines, 67953)
+    "src/resources/CubeConundrum.in"
   putStrLn ""
 
 gearRatiosSolution :: IO ()
 gearRatiosSolution = do
-  putStr "GearRatios one -> "
-  print . sum . partNumbers =<< readFile "src/resources/GearRatios.in"
+  solutionPretty
+    ("GearRatios", 1)
+    (sum . partNumbers, 520019)
+    "src/resources/GearRatios.in"
+  solutionPretty
+    ("GearRatios", 2)
+    (sum . gearRatios, 75519888)
+    "src/resources/GearRatios.in"
   putStrLn ""
+
+solutionPretty :: (Show b, Eq b) => (String, Int) -> (String -> b, b) -> FilePath -> IO ()
+solutionPretty (puzzle, part) (solution, expectedResult) source = do
+  putStr $ unwords [puzzle, show part, "->"] ++ " "
+  print . (assert =<< (== expectedResult)) . solution =<< readFile source
